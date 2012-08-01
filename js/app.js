8144cae662a3d981c4fca757730c31fbfbf525aa
msacca <msacca@tinyfactory.co>
2012-08-01 12:20:12 -0700

    worked on the user login so the user can now put in their dribbble name and see the people they are followings shots - if they delete their username it just shows popular

--- js/app.js
+++ js/app.js
@@ -1,35 +1,51 @@
 var drib = {
    host: 'http://api.dribbble.com',
    popular: '/shots/popular',
-   player: '/player',
+   player: '/players/',
 
    getPopular: function(){
-      var url = drib.host + drib.popular + '?callback=foo';
+      var url = drib.host + drib.popular;
       this.req(url, function(data){
          console.log(data);
          popularShots.buildShots(data.shots);
       });
    },
-   getFollowing: function() {
-      var url = drib.host + drib.popular + '?callback=foo';
+   getFollowing: function(playerId) {
+      var url = drib.host + drib.player + playerId + '/shots/following';
+      this.req(url, function(data) {
+         console.log(data);
+         getFollowingShots.buildList(data.shots);
+      });
    },
    req: function(url, cb) {
       $.ajax({
-         url: url,
-         dataType: 'jsonp'
-      }).done(function(data){
-         cb(data);
-      });
+            url: url,
+            dataType: 'jsonp',
+            timeout: 2000,
+            success: function(data) {  
+               console.log('did the thing');
+               cb(data); 
+            },
+            error: function(x, t, m) {
+               if(t==="timeout") {
+                   console.log('timeout error');
+                   popularShots.buildShots(data.shots);
+               } else {
+                   console.log('else timeout error');
+                   popularShots.buildShots(data.shots);
+               }
+            }
+         }
+    
    }
-}
 
 var popularShots = {
    container:$('#picsList'),
    buildShots: function(shots) {
-      console.log(shots.length);
+      var imgs = '';
       for(var i=0; i<6; i++) {
-         popularShots.container.append(
-            '<li class="animate fadeInUp" style="-webkit-animation-delay: '+ i*150 +'ms;">'+
+         console.log(shots[i]);
+         imgs += '<li class="animate fadeInUp" style="-webkit-animation-delay: '+ i*150 +'ms;">'+
             '    <div class="user">'+
             '        <img src="'+ shots[i].player.avatar_url +'" />'+
             '        <span class="name">'+ shots[i].player.name +'</span>'+
@@ -38,23 +54,63 @@ var popularShots = {
             '        <img class="shot" src="'+ shots[i].image_url +'"/>'+
             '        <a href="'+shots[i].short_url+'"></a>'+ 
             '    </div>'+    
-            '</li>'
-         );
+            '</li>';
       }
+      this.container.html(imgs);
    }
 }
 
-var getFollowing = {
-   container:$('#picsList'),
+var getFollowingShots = {
    buildList: function(shots) {
-      
+      this.container = $('#picsList');
+      var imgs = '';
+      for(var i=0; i<6; i++){
+         console.log(shots[i]);
+         imgs += '<li class="animate fadeInUp" style="-webkit-animation-delay: '+ i*150 +'ms;">'+
+            '    <div class="user">'+
+            '        <img src="'+ shots[i].player.avatar_url +'" />'+
+            '        <span class="name">'+ shots[i].player.name +'</span>'+
+            '    </div>'+    
+            '    <div class="highlight">'+
+            '        <img class="shot" src="'+ shots[i].image_url +'"/>'+
+            '        <a href="'+shots[i].short_url+'"></a>'+ 
+            '    </div>'+    
+            '</li>';
+      }
+      this.container.html(imgs);
+   }
+}
+
+var user = $('#lookup-player');
+var ui = {
+   init: function(){
+      var userName = window.localStorage.getItem('user');
+      if (userName !== null && userName !== '') {
+         user.val(userName);
+         console.log('getFollowing');
+         drib.getFollowing(userName);
+      } else {
+         drib.getPopular();
+      }
+
+      user.blur(function(){
+         var val = $(this).val();
+         if (val == '') {
+            drib.getPopular();
+            return;
+         }
+
+         window.localStorage.setItem('user', $(this).val());
+         drib.getFollowing($(this).val());
+      });
    }
 }
 
 $(function() {
    console.log('initialize');
-   drib.getPopular();
-
+   //drib.getPopular();
+   ui.init();
+   
    $('ul#picsList').on("mouseenter", "li", function(){
             $(this).find('.highlight').children().addClass('reveal');
    });
@@ -62,13 +118,5 @@ $(function() {
    $('ul#picsList').on("mouseleave", "li", function(){
             $(this).find('.highlight').children().removeClass('reveal');
    });
-
-/*   
-   $('ul#picsList li').hover(function(){
-   		$(this).find('.highlight').children().addClass('reveal');
-   } ,function(){
-  		$(this).find('.highlight').children().removeClass('reveal');
-	}); 
-*/
    
 });
