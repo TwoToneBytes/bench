b5c6eeeead7f72fd71ea7d0513dc88b6bdb2071c
msacca <msacca@tinyfactory.co>
2012-08-16 13:23:00 -0700

    final? update

--- js/app.js
+++ js/app.js
@@ -1,20 +1,59 @@
+ var showing = 0;
 var drib = {
    host: 'http://api.dribbble.com',
    popular: '/shots/popular',
+   everyone: '/shots/everyone',
+   debuts: '/shots/debuts',
    player: '/players/',
+
+   
 
    getPopular: function(){
       var url = drib.host + drib.popular;
+      showing = 0;
       this.req(url, function(data){
          console.log(data);
          popularShots.buildShots(data.shots);
+         
       });
    },
+   getEveryone: function(){
+      showing = 1;
+      var url = drib.host + drib.everyone;
+      this.req(url, function(data){
+         console.log(data);
+         popularShots.buildShots(data.shots);
+         
+      });
+   },
+   getDebuts: function(){
+      showing = 2;
+      var url = drib.host + drib.debuts;
+      this.req(url, function(data){
+         console.log(data);
+         popularShots.buildShots(data.shots);
+         
+      });
+   },
+   getPopularNoError: function(){
+      var url = drib.host + drib.popular;
+      showing = 4;
+      this.req(url, function(data){
+         console.log(data);
+         popularShots.buildShots(data.shots);
+         
+      });
+   },
+
    getFollowing: function(playerId) {
+     showing = 3;
+     $('.subInfo').html("<span style='color:#68a4f5'>searching...<span>");
       var url = drib.host + drib.player + playerId + '/shots/following';
       this.req(url, function(data) {
          console.log(data);
+         $('#lookup-player').removeClass('error');
          getFollowingShots.buildList(data.shots);
+         
       });
    },
    req: function(url, cb) {
@@ -23,14 +62,40 @@ var drib = {
             dataType: 'jsonp',
             timeout:   1000,
             success: function(data) {
-               console.log('did the thing');
+               var userName = window.localStorage.getItem('user');
+               console.log('showing '+showing);
+               if(showing == 3) {
+                  console.log('hello showing');
+                  $('.subInfo').html("<span style='color:#81ce4d'>Got it!</span> You can also type 'everyone', 'popular' or 'debuts'");
+                  $('#lookup-player').removeClass().addClass('playerList');
+               } else if(showing == 1) {
+                  console.log('hello everyone');
+                  $('.subInfo').html("<span style='color:#81ce4d'>Everyone it is!</span> You can also input your username, 'popular' or 'debuts'");
+                  $('#lookup-player').removeClass().addClass('everyone');
+               } else if(showing == 2) {
+                  console.log('hello debuts');
+                  $('.subInfo').html("<span style='color:#81ce4d'>We're showing Debuts!</span> You can also input your username, 'debuts' or 'popular'");
+                  $('#lookup-player').removeClass().addClass('debuts');
+               } else if(showing == 4) {
+                  console.log('hello popular');
+                  $('.subInfo').html("<span style='color:#81ce4d'>We're showing Popular!</span> You can also input your username, 'everyone' or 'debuts'");
+                  $('#lookup-player').removeClass().addClass('popular');
+               }
+               
+               if(!$("#lookup-player").val()) {
+                  $('#lookup-player').addClass('error');
+                  $('.subInfo').html("Input your username and we'll show the players you're following or input 'everyone', 'popular' or 'debuts' to see those feeds.  We'll show popular by default");
+               }
                cb(data);
             },
             error: function(x, t, m) {
                if(t==="timeout") {
+                   $('#lookup-player').removeClass('playerList').addClass('error');
+                   $('.subInfo').html("<span style='color:#ce4d73'>We're sorry, we couldn't find that username.  We'll show you the popular shots in the meantime<span>");
                    console.log('timeout error');
                    drib.getPopular();
                } else {
+                   $('#lookup-player').addClass('error');
                    console.log('else timeout error');
                    drib.getPopular();
                }
@@ -117,34 +182,87 @@ var user = $('#lookup-player');
 var ui = {
    init: function(){
       var userName = window.localStorage.getItem('user');
-      if (userName !== null && userName !== '') {
-         user.val(userName);
-         console.log('getFollowing');
-         drib.getFollowing(userName);
-      } else {
-         drib.getPopular();
-      }
-
+      
       user.blur(function(){
          var val = $(this).val();
-         if (val == '') {
+         if (val == 'debuts') {
+            window.localStorage.setItem('user', $(this).val());
+            drib.getDebuts();
+            return;
+         }  else if (val == 'everyone') {
+            window.localStorage.setItem('user', $(this).val());
+            drib.getEveryone();
+            return;
+         } else if (val == 'popular') {
+            window.localStorage.setItem('user', $(this).val());
+            drib.getPopularNoError();
+            return;
+         } else if (val == '') {
+            console.log('setting user to blank');
+            window.localStorage.setItem('user', '');
             drib.getPopular();
             return;
-         }
+         }       
 
          window.localStorage.setItem('user', $(this).val());
          drib.getFollowing($(this).val());
       });
+
+      user.keyup(function(event){
+         if(event.keyCode == 13){   
+            var val = $(this).val();
+            if (val == 'debuts') {
+               window.localStorage.setItem('user', $(this).val());
+               drib.getDebuts();
+               return;
+            }  else if (val == 'everyone') {
+               window.localStorage.setItem('user', $(this).val());
+               drib.getEveryone();
+               return;
+            } else if (val == 'popular') {
+               window.localStorage.setItem('user', $(this).val());
+               drib.getPopularNoError();
+               return;
+            } else if (val == '') {
+               console.log('setting user to blank');
+               window.localStorage.setItem('user', '');
+               drib.getPopular();
+               return;
+            }       
+
+
+            window.localStorage.setItem('user', $(this).val());
+            drib.getFollowing($(this).val());
+         }
+      });    
+
+      if (userName !== null && userName !== '') {
+         user.val(userName);
+         if (userName == 'debuts') {
+            drib.getDebuts();
+            return;
+         }  else if (userName == 'everyone') {
+            drib.getEveryone();
+            return;
+         } else if (userName == 'popular') {
+            drib.getPopularNoError();
+            return;
+         } else {
+            console.log('getFollowing');
+            drib.getFollowing(userName);
+            return;
+         } 
+      } else  {
+         drib.getPopular();
+      }   
+       
    }
 }
 
 $(function() {
    console.log('initialize');
-   //drib.getPopular();
    ui.init();
    
-   /* GO */
-   //Flipper.do('.picture');
 
    $("a.settings").click(function() {
       $(".overlay").fadeIn(250);
@@ -157,13 +275,5 @@ $(function() {
        // "display" : "none"
       });
    });
-
-   /*$('ul#picsList').on("mouseenter", "li", function(){
-            $(this).find('.highlight').children().addClass('reveal');
-   });
-
-   $('ul#picsList').on("mouseleave", "li", function(){
-            $(this).find('.highlight').children().removeClass('reveal');
-   });*/
    
 });
