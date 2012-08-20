ac6265bd68a434509f14442b9968cd36cc4907c5
msacca <msacca@tinyfactory.co>
2012-08-20 10:49:58 -0700

    ok

--- js/app-bak.js
+++ js/app-bak.js
@@ -1,4 +1,4 @@
-var showing = 0;
+ var showing = 0;
 var drib = {
    host: 'http://api.dribbble.com',
    popular: '/shots/popular',
@@ -10,42 +10,37 @@ var drib = {
 
    getPopular: function(){
       var url = drib.host + drib.popular;
-      var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + url + '"') + '&format=json&callback=cbfunc';
       showing = 0;
-      console.log(yql);
-      this.req(yql, function(data){
-         console.log(data.results);
-         popularShots.buildShots(data.query.results.json.shots);
+      this.req(url, function(data){
+         console.log(data);
+         popularShots.buildShots(data.shots);
          
       });
    },
    getEveryone: function(){
       showing = 1;
       var url = drib.host + drib.everyone;
-      var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + url + '"') + '&format=json&callback=cbfunc';
-      this.req(yql, function(data){
+      this.req(url, function(data){
          console.log(data);
-         popularShots.buildShots(data.query.results.json.shots);
+         popularShots.buildShots(data.shots);
          
       });
    },
    getDebuts: function(){
       showing = 2;
       var url = drib.host + drib.debuts;
-      var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + url + '"') + '&format=json&callback=cbfunc';
-      this.req(yql, function(data){
+      this.req(url, function(data){
          console.log(data);
-         popularShots.buildShots(data.query.results.json.shots);
+         popularShots.buildShots(data.shots);
          
       });
    },
    getPopularNoError: function(){
       var url = drib.host + drib.popular;
-      var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + url + '"') + '&format=json&callback=cbfunc';
       showing = 4;
-      this.req(yql, function(data){
+      this.req(url, function(data){
          console.log(data);
-         popularShots.buildShots(data.query.results.json.shots);
+         popularShots.buildShots(data.shots);
          
       });
    },
@@ -54,49 +49,45 @@ var drib = {
      showing = 3;
      $('.subInfo').html("<span style='color:#68a4f5'>searching...<span>");
       var url = drib.host + drib.player + playerId + '/shots/following';
-      var yql = 'https://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from json where url="' + url + '"') + '&format=json&callback=cbfunc';
-      this.req(yql, function(data) {
+      this.req(url, function(data) {
          console.log(data);
          $('#lookup-player').removeClass('error');
-         getFollowingShots.buildList(data.query.results.json.shots);
+         getFollowingShots.buildList(data.shots);
          
       });
    },
-   req: function(url, cbfunc) {
-
+   req: function(url, cb) {
       $.ajax({
             url: url,
             dataType: 'jsonp',
-            jsonp: false,
-            jsonpCallback: 'cbfunc',
             timeout:   1000,
             success: function(data) {
-                  var userName = window.localStorage.getItem('user');
-                  console.log('showing '+showing);
-                  if(showing == 3) {
-                     console.log('hello showing');
-                     $('.subInfo').html("<span style='color:#81ce4d'>Got it!</span> You can also type 'everyone', 'popular' or 'debuts'");
-                     $('#lookup-player').removeClass().addClass('playerList');
-                  } else if(showing == 1) {
-                     console.log('hello everyone');
-                     $('.subInfo').html("<span style='color:#81ce4d'>Everyone it is!</span> You can also input your username, 'popular' or 'debuts'");
-                     $('#lookup-player').removeClass().addClass('everyone');
-                  } else if(showing == 2) {
-                     console.log('hello debuts');
-                     $('.subInfo').html("<span style='color:#81ce4d'>We're showing Debuts!</span> You can also input your username, 'everyone' or 'popular'");
-                     $('#lookup-player').removeClass().addClass('debuts');
-                  } else if(showing == 4) {
-                     console.log('hello popular');
-                     $('.subInfo').html("<span style='color:#81ce4d'>We're showing Popular!</span> You can also input your username, 'everyone' or 'debuts'");
-                     $('#lookup-player').removeClass().addClass('popular');
-                  }
-                  
-                  if(!$("#lookup-player").val()) {
-                     $('#lookup-player').addClass('error');
-                     $('.subInfo').html("Input your username and we'll show the players you're following or input 'everyone', 'popular' or 'debuts' to see those feeds.  We'll show popular by default");
-                  }
-                  cbfunc(data);
-            }, 
+               var userName = window.localStorage.getItem('user');
+               console.log('showing '+showing);
+               if(showing == 3) {
+                  console.log('hello showing');
+                  $('.subInfo').html("<span style='color:#81ce4d'>Got it!</span> You can also type 'everyone', 'popular' or 'debuts'");
+                  $('#lookup-player').removeClass().addClass('playerList');
+               } else if(showing == 1) {
+                  console.log('hello everyone');
+                  $('.subInfo').html("<span style='color:#81ce4d'>Everyone it is!</span> You can also input your username, 'popular' or 'debuts'");
+                  $('#lookup-player').removeClass().addClass('everyone');
+               } else if(showing == 2) {
+                  console.log('hello debuts');
+                  $('.subInfo').html("<span style='color:#81ce4d'>We're showing Debuts!</span> You can also input your username, 'everyone' or 'popular'");
+                  $('#lookup-player').removeClass().addClass('debuts');
+               } else if(showing == 4) {
+                  console.log('hello popular');
+                  $('.subInfo').html("<span style='color:#81ce4d'>We're showing Popular!</span> You can also input your username, 'everyone' or 'debuts'");
+                  $('#lookup-player').removeClass().addClass('popular');
+               }
+               
+               if(!$("#lookup-player").val()) {
+                  $('#lookup-player').addClass('error');
+                  $('.subInfo').html("Input your username and we'll show the players you're following or input 'everyone', 'popular' or 'debuts' to see those feeds.  We'll show popular by default");
+               }
+               cb(data);
+            },
             error: function(x, t, m) {
                if(t==="timeout") {
                    $('#lookup-player').removeClass('playerList').addClass('error');
@@ -112,7 +103,6 @@ var drib = {
          });
       }
    }    
-
 
 var popularShots = {
    container:$('#picsList'),
